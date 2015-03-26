3e6538b8725372bc85aa61ac57b7583aa50d006b
Raúl Jiménez <jimenezjrs@gmail.com>
2015-03-26 17:46:19 -0600

    - Added a column name to users - Profiles needs to be completed to post a complaint - Created two more tests for the complaint functionality

--- spec/business/creates_complaint_spec.rb
+++ spec/business/creates_complaint_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 feature 'A business enters the inspections view' do
-  attr_reader :municipio, :user
+  attr_reader :municipio, :user, :user_with_complete_profile
 
   before do
     @municipio = create :municipio
     @user = create :user
+    @user_with_complete_profile = create :user, :with_profile
   end
 
-  scenario 'and creates a new complaint without a user login' do
+  scenario 'and creates a new complaint without a user logged in' do
     visit municipio_inspections_path(municipio)
     click_on I18n.t('inspections.index.complaint')
 
@@ -18,9 +19,20 @@ feature 'A business enters the inspections view' do
     expect(current_path).to eq new_user_path
   end
 
-  scenario 'and creates a new complaint with a user login' do
+  scenario 'and creates a new complaint with a user logged in WITHOUT a complete profile' do
     # sign in user
     sign_in(user)
+
+    # go to the city new complaint section
+    visit new_municipio_complaint_path(municipio)
+
+    expect(page).to have_content I18n.t('flash.complaints.you_need_to_complete_your_profile')
+    expect(current_path).to eq edit_user_path(user)
+  end
+
+  scenario 'and creates a new complaint with a user logged in WITH a complete profile' do
+    # sign in user
+    sign_in(user_with_complete_profile)
 
     # go to the city new complaint section
     visit new_municipio_complaint_path(municipio)
