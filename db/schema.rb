2a0cee25bbefc758c13744afcc93b6a9fa55bf50
Raúl Jiménez <jimenezjrs@gmail.com>
2015-03-26 13:42:57 -0600

    created a migration to add a user to the complaint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326191444) do
+ActiveRecord::Schema.define(version: 20150326194037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150326191444) do
     t.datetime "updated_at"
     t.integer  "municipio_id"
     t.string   "custom_reason"
+    t.integer  "user_id"
   end
 
   create_table "dependencies", force: true do |t|
