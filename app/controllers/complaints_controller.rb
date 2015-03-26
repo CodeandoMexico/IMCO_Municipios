9314852ac90f69eb25dcb8e114a0f784673db00b
Raúl Jiménez <jimenezjrs@gmail.com>
2015-03-26 11:42:34 -0600

    fix some issues while saving the new complaint

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,17 +1,19 @@
 class ComplaintsController < ApplicationController
   layout 'blanco'
   before_filter :set_municipios
-  before_filter :complaint_parms, only: :create, :update
+  before_filter :complaint_params, only: [:create, :update]
 
   def new
     @complaint = Complaint.new
   end
 
   def create
-    if Complaint.validate_and_create(@municipio, complaint_params)
-
+    @complaint = Complaint.new
+    if @complaint.validate_and_save(@municipio, complaint_params)
+      redirect_to municipio_inspections_path(@municipio),
+        notice: I18n.t('complaints.created_successfully')
     else
-
+      render :new
     end
   end
 
@@ -29,6 +31,10 @@ class ComplaintsController < ApplicationController
   end
 
   def complaint_params
-    params.require(:complaint).permit(:reason, :custom_reason, :description)
+    params.require(:complaint).permit(
+      :reason,
+      :custom_reason,
+      :description
+    )
   end
 end
--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -2,17 +2,22 @@ class Complaint < ActiveRecord::Base
   belongs_to :municipio
 
   def validate_and_save(municipio, args)
+    # This custom method is to sanitize and validate
+    # the answer.
     if args.fetch(:reason) != 'other'
-      self.create(
-        municipio: municipio,
-        reason: COMPLAINTS[args.fetch(:reason)]
-      )
+      reason = COMPLAINTS[args.fetch(:reason)]
+      custom_reason = nil
     else
-      self.create(
-        municipio: municipio,
-        reason: args.fetch(:reason),
-        custom_reason: args.fetch(:custom_reason)
-      )
+      reason = args.fetch(:reason)
+      custom_reason = args.fetch(:custom_reason)
     end
+
+    # update the object content
+    self.municipio_id = municipio.id
+    self.reason = reason
+    self.custom_reason = custom_reason
+    self.description = args.fetch(:description)
+
+    self.save
   end
 end
