2cc43fb2def4c2a44e85ef12d1c7a55ded1474da
Raúl Jiménez <jimenezjrs@gmail.com>
2015-03-27 13:48:42 -0600

    refactor complaint tests to support emails send for users

--- config/application.rb
+++ config/application.rb
@@ -22,17 +22,16 @@ module PruebaDB
     config.i18n.default_locale = :es
 
     # configure mailer
-    config.action_mailer.default_url_options = { host: ENV['ACTION_MAILER_HOST'] }
-    config.action_mailer.smtp_settings = {
-      :user_name => ENV['SMTP_CONFIG_USERNAME'],
-      :password => ENV['SMTP_CONFIG_PASSWORD'],
-      :domain => ENV['SMTP_CONFIG_DOMAIN'],
-      :address => ENV['SMTP_CONFIG_ADDRESS'],
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
-
+      config.action_mailer.default_url_options = { host: ENV['ACTION_MAILER_HOST'] }
+      config.action_mailer.smtp_settings = {
+        :user_name => ENV['SMTP_CONFIG_USERNAME'],
+        :password => ENV['SMTP_CONFIG_PASSWORD'],
+        :domain => ENV['SMTP_CONFIG_DOMAIN'],
+        :address => ENV['SMTP_CONFIG_ADDRESS'],
+        :port => 587,
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
 
     # devise layout settings
     config.to_prepare do
