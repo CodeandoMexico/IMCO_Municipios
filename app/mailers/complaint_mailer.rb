6fc84d6ce46016b0dc4bcc2ee1be8896c4cf5458
Raúl Jiménez <jimenezjrs@gmail.com>
2015-03-27 12:38:41 -0600

    minor changes to the email, and link them with environment variables

--- app/mailers/complaint_mailer.rb
+++ app/mailers/complaint_mailer.rb
@@ -1,15 +1,15 @@
 class ComplaintMailer < ActionMailer::Base
-  default from: "mikesaurio@gmail.com"
+  default from: ENV['MAILER_DEFAULT_FROM']
 
   def send_to_business(user,complaint)
     @user = user
     @complaint = complaint
-    mail(to: @user.email, subject: 'Denuncia enviada correctamente')
+    mail to: @user.email, subject: I18n.t('mailers.complaints.send_to_business_subject')
   end
 
    def send_to_municipio(user,complaint)
     @user = user
     @complaint = complaint
-    mail(to: @user.email, subject: 'Denuncia de inspección recibida')
+    mail to: @user.email, subject: I18n.t('mailers.complaints.send_to_city_subject')
   end
 end
