169ce480ed59d27eefe5e30bca5935c102315954
mikesaurio <mikesaurio@gmail.com>
2015-04-01 18:11:27 -0600

    se agrega municipios dinamicos al landing

--- app/controllers/imcos_controller.rb
+++ app/controllers/imcos_controller.rb
@@ -4,13 +4,13 @@ class ImcosController < ApplicationController
 #before_filter :authenticate_user! 
 
   def index
-    @municipios = Municipio.all
+    @municipios = Municipio.all.order(:nombre)
 
-     if params[:query].present?
-      @results = Municipio.search(params[:query], nombre: params[:nombre])
-    else
-      @results = Municipio.all
-    end
+   #  if params[:query].present?
+     # @results = Municipio.search(params[:query], nombre: params[:nombre])
+    #else
+     # @results = Municipio.all
+   # end
 
   end
 end
