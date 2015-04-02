b56f6bc421b43745ed99dffd9ea3609994763c66
mikesaurio <mikesaurio@gmail.com>
2015-04-02 00:19:44 -0600

    se agregan traducciones y mensajes

--- app/mailers/complaint_mailer.rb
+++ app/mailers/complaint_mailer.rb
@@ -8,6 +8,13 @@ class ComplaintMailer < ActionMailer::Base
 
    def send_to_municipio(complaint)
     @complaint = complaint
-    mail to: @complaint.municipio.contact_email, subject: I18n.t('mailers.complaints.send_to_city_subject')
+    if @complaint.municipio.contact_email.nil? || @complaint.municipio.contact_email.empty?
+        User.all.where(municipio_id: complaint.municipio, admin: true).each do |admin|
+          mail to: admin.email, subject: I18n.t('mailers.complaints.send_to_city_subject')
+        end
+    else
+      mail to: @complaint.municipio.contact_email, subject: I18n.t('mailers.complaints.send_to_city_subject')
+    end
+    
   end
 end
