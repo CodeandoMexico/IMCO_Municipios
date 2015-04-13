381a26940588b48b16c91b6c026e44cb7c16d14d
mikesaurio <mikesaurio@gmail.com>
2015-04-12 19:13:36 -0500

    test inspectors

--- spec/features/dashboard/create_and_edit_formation_steps_spec.rb
+++ spec/features/dashboard/create_and_edit_formation_steps_spec.rb
@@ -36,17 +36,18 @@ feature 'validate Formation_Steps' do
     fill_in 'formation_step[name]', with: 'new name of formation step'
     fill_in 'formation_step[description]', with: 'description of formation step'
     fill_in 'formation_step[path]', with: 'http://pagina_path.php'
-    fill_in 'formation_step[type_formation_step]', with: 'AF'
+    fill_in 'formation_step[type_formation_step]', with: 'AM'
 
     click_on I18n.t('dashboard.formation_steps.form.create_formation_steps')
     visit dashboard_formation_steps_path
     expect(page).to have_content 'new name of formation step'
+    expect(page).to have_content 'Moral'
   end
 
   def create_formation_step
     click_on I18n.t('dashboard.formation_steps.index.new_formation_step')
     
-    fill_in 'formation_step[name]', with: 'new name of formation step'
+    fill_in 'formation_step[name]', with: 'Name of formation step'
     fill_in 'formation_step[description]', with: 'description of formation step'
     fill_in 'formation_step[path]', with: 'http://pagina_path.php'
     fill_in 'formation_step[type_formation_step]', with: 'AF'
