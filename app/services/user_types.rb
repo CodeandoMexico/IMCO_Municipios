ac861954ec25fc41498597e8e744abbd7fb19c77
mikesaurio <mikesaurio@gmail.com>
2015-05-12 17:03:59 -0500

    procedures Crud

--- app/services/user_types.rb
+++ app/services/user_types.rb
@@ -1,5 +1,12 @@
 module UserTypes 
   def self.type_of_people_formation_step
-    ['AM' ,  'AF']
+    ['AM' ,  'AF','A']
+  end
+
+  def self.type_of_people_procedures
+    ['TM' ,  'TF','A']
+  end
+  def self.type_of_sare
+        ['0','1']
   end
 end 
