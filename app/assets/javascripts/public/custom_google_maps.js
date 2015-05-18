f289862d52ca58c2801b8858ea1c3b2e57e285d1
Raúl Jiménez <jimenezjrs@gmail.com>
2015-05-18 18:57:26 -0500

    refacotr the custom google maps javascript

--- app/assets/javascripts/public/custom_google_maps.js
+++ app/assets/javascripts/public/custom_google_maps.js
@@ -1,10 +1,48 @@
 // This function is the one that will render th gmap
 
-var loadGoogleMaps = function(markerData){
-  var handler = Gmaps.build('Google');
-  handler.buildMap({ internal: {id: 'gmap'}}, function(){
-    var markers = handler.addMarkers(markerData);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
+var map;
+var markers = [];
+
+function initialize(markerData) {
+  // get center point
+  var centerPoint = fetchPoint(22.1498200, -100.9791600);
+
+  // paint a map into the view
+  var mapOptions = {
+    zoom: 4,
+    center: centerPoint,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+
+  // load all marker into the array from locations
+  var new_marker;
+  for (var i = 0; i < markerData.length; i++){
+    new_point = new google.maps.LatLng(markerData[i]["lat"], markerData[i]["lng"]);
+    addMarker(new_point);
+  }
+}
+
+// Sets the map on all markers in the array.
+var setAllMap = function(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+var clearMarkers = function() {
+  setAllMap(null);
+}
+
+var addMarker = function(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
   });
+  markers.push(marker);
 };
+
+var fetchPoint = function(lat, lng){
+  return new google.maps.LatLng(lat, lng);
+}
