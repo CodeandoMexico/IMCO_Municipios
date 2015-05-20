9833d99dbf5c9a335fb48158edcba3df9d9cfe52
mikesaurio <mikesaurio@gmail.com>
2015-05-20 09:08:54 -0500

    se soluciona heroku maps

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'csv'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -43,5 +42,6 @@ module PruebaDB
     end
 
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
