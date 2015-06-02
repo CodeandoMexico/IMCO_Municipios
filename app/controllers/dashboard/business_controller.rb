27aa646fe92d4954e1654ac5f37e6510414d3ad5
mikesaurio <mikesaurio@gmail.com>
2015-06-02 15:29:26 -0500

    se puede ver la info completa de una empresa

--- app/controllers/dashboard/business_controller.rb
+++ app/controllers/dashboard/business_controller.rb
@@ -1,25 +1,26 @@
 module Dashboard
   class BusinessController < ApplicationController
-    before_filter :set_user, :set_city
+    before_filter  :set_city
+    before_action :set_user, only: [:show]
     layout 'dashboard'
 
     def index
       redirect_to root_path if current_user.nil?
-      @users =  User.where(city_id: @city.id, admin: false)
     end
 
-    def edit
+    def show
     end
 
 
     private
 
     def set_user
-      @user ||= current_user
+      @user =  User.find(params[:id])
     end
 
     def set_city
       @city ||= City.find(current_user.city_id)
+      @users =  User.where(city_id: @city.id, admin: false)
     end
   end
 end
