6e2745fb16324873124f820440530c56f4b8916d
mikesaurio <mikesaurio@gmail.com>
2015-06-04 13:26:43 -0500

    se implementa ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
     redirect_to root_path if current_user.nil?
 
     @user = current_user
+    
     unless @user.nil?
       unless @user.city_id.nil?
         @city_select = City.find(@user.city_id).id
@@ -49,6 +50,10 @@ class UsersController < ApplicationController
         @lines = Line.where(city_id: params[:pagetime][:city_id])
         fill_lines(params[:pagetime][:city_id])
       end
+      @phone = @user.phone
+      @schedule = @user.schedule
+      @bussine_name = @user.business_name
+      @user_operation_license = @user.operation_license
     end
   end
 
@@ -125,16 +130,6 @@ class UsersController < ApplicationController
 
   def set_city
     @city = City.find(params[:city_id])
-  end
-
-  def fill_lines(id)
-    @array_line=[]
-    @array_id=[]
-    @city_select = id
-    Line.order(:name).where(city_id: id).select(:name, :id).each_with_index do |value, index|
-        @array_line[index] = value.name.to_s
-        @array_id[index] = value.id.to_s;
-    end
   end
 
 end
