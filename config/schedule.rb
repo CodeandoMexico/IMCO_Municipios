dfc304e53924fd07661c2d962e3bf35b4a49d6d8
mikesaurio <mikesaurio@gmail.com>
2015-06-10 12:09:33 -0500

    se deja tarea solo una vez al dia

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,3 @@ set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log
 every 1.day, :at => '6:30 am' do
   rake "mail_tasks:send_reminder"
 end
-
-every 15.minutes do
-  rake "mail_tasks:send_reminder"
-end
