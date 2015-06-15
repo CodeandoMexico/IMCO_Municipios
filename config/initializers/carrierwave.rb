14a38e5ed9d25bcc107d6f1b1c8729da030c27cd
mikesaurio <mikesaurio@gmail.com>
2015-06-15 13:02:48 -0500

    se modifica configuracion para subir fotos

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AMAZON_ACCESS_KEY'],                        # required
-    :aws_secret_access_key  => ENV['AMAZON_SECRET_KEY'],                        # required
-    :region                 => ENV['AMAZON_REGION'],                  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = ENV['S3_BUCKET']                          # required
-  config.fog_public     = true
-  config.permissions = 0600
-  config.storage(Rails.env.test? || Rails.env.development? ? :file : :fog)
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AMAZON_ACCESS_KEY'],                        # required
+      :aws_secret_access_key  => ENV['AMAZON_SECRET_KEY'],                        # required
+      :region                 => ENV['AMAZON_REGION'],                  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = ENV['S3_BUCKET']                          # required
+    config.fog_public     = true
+    config.permissions = 0600
+  end
 end
