43e295f9c15fcbde36f42c1277db47f097421d80
mikesaurio <mikesaurio@gmail.com>
2015-06-23 15:24:43 -0500

    se aregla prueba dependencia

--- spec/features/dashboard/create_and_edit_dependencies_spec.rb
+++ spec/features/dashboard/create_and_edit_dependencies_spec.rb
@@ -14,22 +14,17 @@ feature 'validate Dependency' do
     create_dependency
   end
 
-  scenario 'and edit dependency' do
-    sign_in admin
-
-    visit dashboard_path(admin)
-    #save_and_open_page
-    click_on I18n.t('dashboard.menu.dependencies')
-    
-    create_dependency
-    edit_dependency
-  end
+   scenario 'and edit dependency' do
+     sign_in admin
+     visit dashboard_path(admin)
+     click_on I18n.t('dashboard.menu.dependencies')
+     create_dependency
+     edit_dependency
+   end
 
 
   def edit_dependency
-    visit dashboard_dependencies_path
-    click_on I18n.t('form.actions.edit')
-
+    visit edit_dashboard_dependency_path(Dependency.last)
     fill_in 'dependency[name]', with: 'new name of dependency'
     click_on I18n.t('dashboard.dependencies.form.create_dependency')
     visit dashboard_dependencies_path
@@ -38,10 +33,10 @@ feature 'validate Dependency' do
 
   def create_dependency
     click_on I18n.t('dashboard.dependencies.index.new_dependency')
-    
     fill_in 'dependency[name]', with: 'Name of dependency'
     click_on I18n.t('dashboard.dependencies.form.create_dependency')
     visit dashboard_dependencies_path
-    expect(page).to have_content 'Name of dependency'
+    expect(page).to have_text('Name of dependency')
+    #save_and_open_page
   end
 end
