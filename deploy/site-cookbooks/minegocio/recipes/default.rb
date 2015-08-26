2c14afdbabd584ac58da785e37c98a0cc5e46493
mikesaurio <mikesaurio@gmail.com>
2015-08-26 17:00:33 -0500

    se crea banner con estilos

--- deploy/site-cookbooks/minegocio/recipes/default.rb
+++ deploy/site-cookbooks/minegocio/recipes/default.rb
@@ -12,9 +12,9 @@ creds.keys.each do |key|
 end
 
     
-docker_service 'default' do
-    action [:create, :start]
-end
+#docker_service 'default' do
+#    action [:create, :start]
+#end
 
 
 docker_image "docker.io/postgres" do
@@ -22,16 +22,16 @@ docker_image "docker.io/postgres" do
 end
 
 docker_image "phusion/passenger-ruby22" do
-    tag "latest"
+    tag "0.9.16"
 end
 
-docker_container "postgres" do
-    image "docker.io/postgres"
-    tag "9.4"
-    container_name "postgres"
-    detach  true
-    env "POSTGRES_PASSWORD=#{creds['POSTGRES_PASSWORD']}"
-end
+#docker_container "postgres" do
+#    image "docker.io/postgres"
+#    tag "9.4"
+#    container_name "postgres"
+#    detach  true
+#    env "POSTGRES_PASSWORD=#{creds['POSTGRES_PASSWORD']}"
+#end
 
 
 git '/var/minegocio' do
@@ -44,10 +44,34 @@ end
 docker_image "minegocio" do
     tag "latest"
     source "/var/minegocio"
+    cmd_timeout 12200
     action :build
-    notifies :redeploy, "docker_container[minegocio_sidekiq]", :immediately
     notifies :redeploy, "docker_container[minegocio_app]", :immediately
 end
+
+#docker_container "minegocio_dbcreate" do
+#    image "minegocio"
+#    tag "latest"
+#    container_name "minegocio_dbcreate"
+#    env list_credentials
+#    link ["postgres:postgres"]
+#    entrypoint "rake"
+#    command "db:create"
+#    remove_automatically true
+#    action :run
+#end
+
+#docker_container "minegocio_dbmigrate" do
+#    image "minegocio"
+#    tag "latest"
+#    container_name "minegocio_dbmigrate"
+#    env list_credentials
+#    link ["postgres:postgres"]
+#    entrypoint "rake"
+#    command "db:migrate"
+#    remove_automatically true
+#   action :run
+#end
 
 docker_container "minegocio_app" do
     image "minegocio"
@@ -56,16 +80,18 @@ docker_container "minegocio_app" do
     detach true
     env list_credentials
     port ["80:80"]
-    links ["postgres:postgres"]
-end
-
-docker_container "minegocio_sidekiq" do
-    image "minegocio"
-    tag "latest"
-    container_name "minegocio_sidekiq"
-    detach true
-    env list_credentials
-    entrypoint "sidekiq"
-    links ["postgres:postgres"]
+    link ["minegociomx_db_1:postgres"]
     action :run
 end
+
+#docker_container "minegocio_cronjob" do
+#    image "minegocio"
+#    tag "latest"
+#    container_name "minegocio_cronjob"
+#    detach true
+#    env list_credentials
+#    entrypoint "sidekiq"
+#    command "db:migrate"
+#    link ["postgres:postgres"]
+#    action :redeploy
+#end
