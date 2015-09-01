c716a8a62ef5691e9b59b49cb033327b95754fdb
mikesaurio <mikesaurio@gmail.com>
2015-09-01 16:23:46 -0500

    se crea una bandera para saber que negocio esta activo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828162646) do
+ActiveRecord::Schema.define(version: 20150901193909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150828162646) do
     t.string   "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "using",                  default: false
   end
 
   create_table "cities", force: true do |t|
