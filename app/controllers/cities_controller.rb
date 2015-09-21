eadd47c64a1020a9e19212cb8415131cd86e2f22
mikesaurio <mikesaurio@gmail.com>
2015-09-21 16:55:56 -0500

    se modifica vista para subir csv's

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -70,6 +70,7 @@ class CitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
-      params.require(:city).permit(:name, :contact_email, :privacy_file, :contact_phone,:regulations_path, :construction_file, :land_file, :business_file)
+      params.require(:city).permit(:name, :contact_email, :privacy_file, :contact_phone,:regulations_path, :construction_file, :land_file, :business_file,
+        :dependency_file, :line_file, :formation_step_file, :requirement_file, :procedure_file, :inspection_file, :inspector_file)
     end
   end
