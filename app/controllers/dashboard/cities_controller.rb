5a9c8b67fdb892e7c77ac9c2b5bbd42d1fe2d8ac
mikesaurio <mikesaurio@gmail.com>
2015-10-19 17:11:54 -0500

    se agrega dashboard para modificar los municipios

--- app/controllers/dashboard/cities_controller.rb
+++ app/controllers/dashboard/cities_controller.rb
@@ -10,6 +10,11 @@ module Dashboard
     end
 
     def edit
+      @city = City.find(params[:id])
+    end
+
+    def new
+      @city = City.new
     end
 
 
@@ -19,9 +24,9 @@ module Dashboard
     def set_city
       @cities = City.all
       @user = current_user
-     # @city ||= City.find(current_user.city_id)
     end
-        # Never trust parameters from the scary internet, only allow the white list through.
+
+
     def city_params
       params.require(:city).permit(:name, :contact_email, :privacy_file, :contact_phone,:regulations_path, :construction_file, :land_file, :business_file,
         :dependency_file, :line_file, :formation_step_file, :requirement_file, :procedure_file, :inspection_file, :inspector_file)
