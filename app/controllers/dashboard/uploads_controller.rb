f57764f0dd6f8b43211b78b328178cf063d25e03
mikesaurio <mikesaurio@gmail.com>
2015-10-20 20:46:44 -0500

    se valida error de loop al subir dataset

--- app/controllers/dashboard/uploads_controller.rb
+++ app/controllers/dashboard/uploads_controller.rb
@@ -65,6 +65,9 @@ module Dashboard
     def set_status
       unless Uploads.where(id_user: current_user.id).blank?
         @status = Uploads.where(id_user: current_user).last
+        if @status.created_at < Time.now - 15.minutes
+          @status.delete_all
+        end
       end
       @dialog = false
       @logs = false
