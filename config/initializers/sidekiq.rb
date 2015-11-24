68bb96d8773c69c2f4e9cd6f41a0f6efe3e26f60
poguez <zeugop@gmail.com>
2015-11-24 12:41:40 -0600

    Se cambia archivo de configuracion de redis por conflicto

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,18 @@
-if Rails.env.production? || Rails.env.development?
+  def docker_redis_url
+    if ENV['REDIS_PORT_6379_TCP_ADDR'] and ENV['REDIS_PORT_6379_TCP_PORT']
+      return "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/0"
+    end
+    nil
+  end
+
   Sidekiq.configure_server do |config|
-    config.redis = { size: 3 }
-    
+    if docker_redis_url
+      config.redis = { url: docker_redis_url, size: 3 }
+    else
+      config.redis = { url: ENV["REDISCLOUD_URL"], size: 3 }
+    end
+
+
     database_url = ENV['DATABASE_URL']
     if database_url
       ENV['DATABASE_URL'] = "#{database_url}?pool=20"
@@ -10,6 +21,9 @@ if Rails.env.production? || Rails.env.development?
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { size: 3 }
+    if docker_redis_url
+      config.redis = { url: docker_redis_url, size: 3 }
+    else
+      config.redis = { size: 3 }
+    end
   end
-end
