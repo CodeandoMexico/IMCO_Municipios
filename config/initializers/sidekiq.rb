b84737df063e8e40e26b11dc6b68ac2aea16beed
poguez <zeugop@gmail.com>
2015-11-24 13:49:23 -0600

    sidekick lee una URL para la base de datos de redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,6 @@
-  def docker_redis_url
-    if ENV['REDIS_PORT_6379_TCP_ADDR'] and ENV['REDIS_PORT_6379_TCP_PORT']
-      return "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}/0"
-    end
-    nil
-  end
-
+if Rails.env.production? || Rails.env.development?
   Sidekiq.configure_server do |config|
-    if docker_redis_url
-      config.redis = { url: docker_redis_url, size: 3 }
-    else
-      config.redis = { url: ENV["REDISCLOUD_URL"], size: 3 }
-    end
-
+    config.redis = { size: 3 }
 
     database_url = ENV['DATABASE_URL']
     if database_url
@@ -21,9 +10,6 @@
   end
 
   Sidekiq.configure_client do |config|
-    if docker_redis_url
-      config.redis = { url: docker_redis_url, size: 3 }
-    else
-      config.redis = { size: 3 }
-    end
+    config.redis = { size: 3 }
   end
+end
