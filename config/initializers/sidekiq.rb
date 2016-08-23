33cc995c43f47c36ce9a37ce35834bf03588e669
mikesaurio <mikesaurio@gmail.com>
2016-08-23 15:49:00 -0500

    se modifica sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,3 @@
-if Rails.env.production? || Rails.env.development?
-  Sidekiq.configure_server do |config|
-    config.redis = { size: 27 }
-    
-    database_url = ENV['DATABASE_URL']
-    if database_url
-      ENV['DATABASE_URL'] = "#{database_url}?pool=20"
-      ActiveRecord::Base.establish_connection
-    end
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = { size: 27 }
-  end
+if ENV["REDISCLOUD_URL"]
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
