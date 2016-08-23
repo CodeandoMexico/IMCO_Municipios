717659abcdd52bce35f8d3ef1df1ff022e6696dd
mikesaurio <mikesaurio@gmail.com>
2016-08-23 16:07:06 -0500

    se crea archivo redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,19 @@
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = { size: 27 }
+    
+    database_url = ENV['DATABASE_URL']
+    if database_url
+      ENV['DATABASE_URL'] = "#{database_url}?pool=20"
+      ActiveRecord::Base.establish_connection
+    end
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { size: 27 }
+  end
+end
+
 if ENV["REDISCLOUD_URL"]
     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
